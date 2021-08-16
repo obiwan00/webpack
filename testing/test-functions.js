b2564fe77e3d4fdcafba33807728e9c6f90f3efd
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2021-08-16 16:53:27 +0300

    Add several unnecessary tests

---
+++ testing/test-functions.js
@@ -0,0 +1,28 @@
+function sum(...args) {
+  return args.reduce((acc, el) => acc + el, 0)
+}
+
+function fibonacci(depth) {
+  let n1 = 0
+  let n2 = 1
+  const result = []
+  let nextTerm;
+
+  for (let i = 1; i <= depth; i++) {
+    result.push(n1)
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  return result
+}
+
+function pow(base, exponent) {
+  return base ** exponent
+}
+
+module.exports = {
+  sum,
+  fibonacci,
+  pow
+}
---
+++ testing/test-functions.spec.js
@@ -0,0 +1,19 @@
+const {sum, fibonacci, pow} = require('./test-functions')
+
+describe('Test custom functions implementation', () => {
+  it('sum', () => {
+    expect(sum(1, 2, 3)).toBe(6)
+    expect(sum(2, 2)).not.toBe(5)
+  })
+  it('fibonacci', () => {
+    expect(fibonacci(6)).toEqual([0, 1, 1, 2, 3, 5])
+    expect(fibonacci(5)).toHaveLength(5)
+  })
+  it('pow', () => {
+    expect(pow(2, -1)).toBeCloseTo(0.5)
+    expect(pow(2,4)).toBe(16)
+    expect(pow(5,3)).toBe(125)
+    expect(pow(-5,3)).toBe(-125)
+    expect(pow(-5,2)).toBe(25)
+  })
+})
